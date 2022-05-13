6be2d1366ceefbe7ecdd616df0dfab71320078a3
gauextra <76999850+gauextra@users.noreply.github.com>
2022-05-13 18:38:38 +0200

    problem while adding new object in arraylist.add(new Event() //constructor)

--- app/src/main/java/com/example/wochenendeplanner/MainActivity.java
+++ app/src/main/java/com/example/wochenendeplanner/MainActivity.java
@@ -4,45 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.ActionCodeSettings;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthProvider;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthSettings;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GithubAuthProvider;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.auth.PlayGamesAuthProvider;
-import com.google.firebase.auth.SignInMethodQueryResult;
-import com.google.firebase.auth.UserInfo;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import Controller.Event;
-import View.EventView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
     listview = findViewById(R.id.Listview);
 
-    ArrayList<Event> arrayList = new ArrayList<>();
-    arrayList.add(new Event(R.drawable.item1, "test",))
+    ArrayList<Event> arrayList = new ArrayList<Event>();
+    arrayList.add(new Event(5, "test",20-05-2022,"test","Weingarten"));
+    arrayList.add(new Event();
+    arrayList.add(new Event( 51, "test3", 3-05-2022, "test3", "We"));
+    arrayList.add(new Event( 54, "test4", 2-05-2022, "test4", "Wein"));
+
 
