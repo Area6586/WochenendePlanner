c3440b6290de0085e0e8c10074a274e07d13d037
Gtam <umt-sch-27@mail.de>
2022-06-08 14:35:51 +0200

    clear error at dublicate resources

--- app/src/main/java/com/example/wochenendeplanner/MainActivity.java
+++ app/src/main/java/com/example/wochenendeplanner/MainActivity.java
@@ -20,13 +20,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    listview = findViewById(R.id.Listview);
-
-    ArrayList<Event> arrayList = new ArrayList<Event>();
-    arrayList.add(new Event(5, "test",20-05-2022,"test","Weingarten"));
-    arrayList.add(new Event();
-    arrayList.add(new Event( 51, "test3", 3-05-2022, "test3", "We"));
-    arrayList.add(new Event( 54, "test4", 2-05-2022, "test4", "Wein"));
-
+}
 
