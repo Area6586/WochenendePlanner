b7a91760850b058f8ac0cf6040531da3461d78a0
Gtam <umt-sch-27@mail.de>
2022-06-11 10:19:44 +0200

    working in EventCreateView

--- app/src/main/java/View/EventCreateView.java
+++ app/src/main/java/View/EventCreateView.java
@@ -1,6 +1,26 @@
 package View;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
 public class EventCreateView {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+
+
+
+
+
 
     Boolean createEvent(){
 
