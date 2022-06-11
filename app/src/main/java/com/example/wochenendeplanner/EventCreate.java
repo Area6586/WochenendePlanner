fa154907709042fc943adba7563ad2c059fe6fc0
Gtam <umt-sch-27@mail.de>
2022-06-11 15:46:03 +0200

    working in EventCreateView & event_details.xml

--- app/src/main/java/com/example/wochenendeplanner/EventCreate.java
+++ app/src/main/java/com/example/wochenendeplanner/EventCreate.java
@@ -7,24 +7,45 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.DateFormat;
 
-public class EventCreate extends AppCompatActivity {
 
-    private DatePicker datePicker;
-    private Calendar calendar;
-    private TextView dateView;
-    private int year, month, day;
+public class EventCreate extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    TextView tvDate;
+    Button btPickDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_create);
+        tvDate = findViewById(R.id.tvDate);
+        btPickDate = findViewById(R.id.btPickDate);
+        btPickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Please note that use your package name here
+                com.example.wochenendeplanner.DatePicker mDatePickerDialogFragment;
+                mDatePickerDialogFragment = new com.example.wochenendeplanner.DatePicker();
+                mDatePickerDialogFragment.show(getSupportFragmentManager(), "DATE PICK");
+            }
+        });
+    }
 
-        dateView = (TextView) findViewById(R.id.date_text);
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        showDate(year, month+1, day);
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+        tvDate.setText(selectedDate);
     }
 }
